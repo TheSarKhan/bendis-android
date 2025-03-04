9ace62fbbc64c8b01f6a27a800c7d68be9a5990b
acer <edileallahverdiyeva480@gmail.com>
2025-03-04 22:11:02 +0400

    Profil page

--- app/src/main/java/com/bendis/ui/view/ProfileFragment.kt
+++ app/src/main/java/com/bendis/ui/view/ProfileFragment.kt
@@ -5,12 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.bendis.R
 import com.bendis.databinding.FragmentProfileBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View{
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val birthdayLayout = view.findViewById<TextInputLayout>(R.id.textInputLayout3)
+
+        birthdayLayout.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.arrow)
+
+        val endIconView = birthdayLayout.findViewById<ImageView>(com.google.android.material.R.id.text_input_end_icon)
+        endIconView.layoutParams = ViewGroup.LayoutParams(20, 20)
+
+        endIconView.requestLayout()
+    }
+
 }
